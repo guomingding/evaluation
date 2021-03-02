8f6eb8b2861dd0c982be511a730c863af7cc8263
guomingding <guomingding@outlook.com>
2021-03-02 14:24:13 +0800

    close CORS

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__, static_folder='../dist/static',
             template_folder='../dist')
-CORS(app, supports_credentials=True)  
+# CORS(app, supports_credentials=True)  
 
 @app.route('/')
 def index():
